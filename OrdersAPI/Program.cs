3df328bbfdf88495a5df8cfcc549098ec3b99fc0
gustavooarantes <dev.gustavoa@gmail.com>
2026-01-04 11:49:25 -0300

    feat: completed the minimal api setup

--- OrdersAPI/Program.cs
+++ OrdersAPI/Program.cs
@@ -1,44 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AppDbContext>(
+		opt => opt.UseSqlite(builder
+			.Configuration.GetConnectionString("BaseConnection")));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.MapPost("api/orders", async (AppDbContext context, Order order) =>
+		{
+			await context.Orders.AddAsync(order);
+			await context.SaveChangesAsync();
 
-app.UseHttpsRedirection();
+			return Results.Created($"/api/orders/{order.Id}", order);
+		});
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.MapGet("api/orders/{id}", async (AppDbContext context, int id) =>
+		{
+			var order = await context
+				.Orders.FirstOrDefaultAsync(o => o.Id == id);
+			if (order == null)
+				return Results.NotFound();
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+			return Results.Ok(order);
+		});
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
